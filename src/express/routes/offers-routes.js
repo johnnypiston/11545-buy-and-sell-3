ec5f35a0ae699020258abc543fa861314dc459ff
Sergey <sergeyfilatov.j@gmail.com>
2021-08-04 11:42:38 +0400

    Добавить обработку динамического параметра id для роута offers

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -3,9 +3,14 @@
 const {Router} = require(`express`);
 const offersRouter = new Router();
 
+offersRouter.param(`id`, (request, response, next, id) => {
+  request.id = id;
+  next();
+});
+
 offersRouter.get(`/add`, (request, response) => response.send(`/offers/add`));
-offersRouter.get(`/:id`, (request, response) => response.send(`/offers/:id`));
-offersRouter.get(`/edit/:id`, (request, response) => response.send(`/offers/edit/:id`));
-offersRouter.get(`/category/:id`, (request, response) => response.send(`/offers/category/:id`));
+offersRouter.get(`/:id`, (request, response) => response.send(`/offers/${request.id}`));
+offersRouter.get(`/edit/:id`, (request, response) => response.send(`/offers/edit/${request.id}`));
+offersRouter.get(`/category/:id`, (request, response) => response.send(`/offers/category/${request.id}`));
 
 module.exports = offersRouter;
