7d64e465fcd9edcf5885c4c88556569fe2a84470
Sergey <sergeyfilatov.j@gmail.com>
2021-08-30 11:14:34 +0400

    Добавить мидлвары для обработки 404 и 500 ошибок

--- src/express/index.js
+++ src/express/index.js
@@ -4,6 +4,7 @@ const chalk = require(`chalk`);
 const express = require(`express`);
 const path = require(`path`);
 const router = require(`./routes`);
+const {HttpResponseCode} = require(`../constants.js`);
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
 const TEMPLATES_DIR = `templates`;
@@ -15,5 +16,13 @@ app.use(router);
 
 app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
 app.set(`view engine`, `pug`);
+
+app.use((req, res) => {
+  res.status(HttpResponseCode.NOT_FOUND).render(`errors/404`);
+});
+app.use((err, req, res) => {
+  console.error(chalk.red(`Ошибка: ${err}`));
+  res.status(HttpResponseCode.INTERNAL_SERVER_ERROR).render(`errors/500`);
+});
 
 app.listen(DEFAULT_PORT, () => console.log(chalk.blue(`Сервер запущен на порту ${DEFAULT_PORT}`)));
